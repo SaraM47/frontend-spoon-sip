b1f0a39992996707941147e8a95e31c0d05f5605
SaraMsoso <Sara.soder44@gmail.com>
2025-06-21 21:59:24 +0200

    Moved load homepage reviews into a separate file without affecting other pages

---
+++ src/script/reviews-home.ts
@@ -0,0 +1,7 @@
+// Dynamically loads customer reviews.
+import { loadHomepageReviews } from './reviews';
+
+  // Load general homepage reviews
+window.addEventListener('DOMContentLoaded', () => {
+  loadHomepageReviews();
+});
