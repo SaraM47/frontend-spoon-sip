654ec00a3d26882cb196be951bd74ae18d8c2792
SaraMsoso <Sara.soder44@gmail.com>
2025-06-24 20:22:06 +0200

    Configured Vite for multi-page build and add Netlify _redirects for routing support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', 
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        menu: 'menu.html',
+        takeaway: 'takeaway.html',
+        ourstory: 'ourstory.html',
+        login: 'login.html',
+        register: 'register.html',
+        admin: 'admin.html'
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', 
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
