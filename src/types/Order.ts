478bf169349296e2bda635821873a5eae72a2f47
SaraMsoso <Sara.soder44@gmail.com>
2025-06-24 19:19:43 +0200

    Added optional 'people' and 'note' fields, populate menuItemIds with MenuItem[]

--- src/types/Order.ts
+++ src/types/Order.ts
@@ -4,8 +4,10 @@ export type Order = {
     _id: string;
     customerName: string;
     phone: string;
-    time: string; // ISO 8601-format
-    menuItemIds: MenuItem[]; // Populerade menyobjekt från backend
+    time: string;
+    people?: number;
+    note?: string;
+    menuItemIds: MenuItem[]; // Populated menu items from the backend
     status: 'pending' | 'completed';
     createdAt: string;
     updatedAt: string;
