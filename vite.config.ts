fde211391a90fba05adde2db0c8f920316227c0f
SaraMsoso <Sara.soder44@gmail.com>
2025-06-26 13:06:32 +0200

    Formatting the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, 'menu.html'),
-        takeaway: resolve(__dirname, 'takeaway.html'),
-        ourstory: resolve(__dirname, 'ourstory.html'),
-        login: resolve(__dirname, 'login.html'),
-        register: resolve(__dirname, 'register.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        main: resolve(__dirname, "index.html"),
+        menu: resolve(__dirname, "src/pages/menu.html"),
+        takeaway: resolve(__dirname, "src/pages/takeaway.html"),
+        ourstory: resolve(__dirname, "src/pages/ourstory.html"),
+        login: resolve(__dirname, "src/pages/login.html"),
+        register: resolve(__dirname, "src/pages/register.html"),
+        admin: resolve(__dirname, "src/pages/admin.html"),
       },
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
