aef9655a8c12a9344bc17627756c3cfa42d2c2c1
SaraMsoso <Sara.soder44@gmail.com>
2025-06-26 13:29:29 +0200

    Removed unnessesary parentheses

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: ('index.html'),
-        menu: ('src/pages/menu.html'),
-        takeaway: ('src/pages/takeaway.html'),
-        ourstory: ('src/pages/ourstory.html'),
-        login: ('src/pages/login.html'),
-        register: ('src/pages/register.html'),
-        admin: ('src/pages/admin.html'),
+        main: 'index.html',
+        menu: 'src/pages/menu.html',
+        takeaway: 'src/pages/takeaway.html',
+        ourstory: 'src/pages/ourstory.html',
+        login: 'src/pages/login.html',
+        register: 'src/pages/register.html',
+        admin: 'src/pages/admin.html',
       },
     },
   },
