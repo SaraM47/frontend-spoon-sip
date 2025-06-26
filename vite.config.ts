d677d8b5fc517d11e883cc25fabb07fb1cb156f1
SaraMsoso <Sara.soder44@gmail.com>
2025-06-26 13:23:39 +0200

    Updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,17 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, 'src/pages/menu.html'),
-        takeaway: resolve(__dirname, 'src/pages/takeaway.html'),
-        ourstory: resolve(__dirname, 'src/pages/ourstory.html'),
-        login: resolve(__dirname, 'src/pages/login.html'),
-        register: resolve(__dirname, 'src/pages/register.html'),
-        admin: resolve(__dirname, 'src/pages/admin.html'),
+        main: ('index.html'),
+        menu: ('src/pages/menu.html'),
+        takeaway: ('src/pages/takeaway.html'),
+        ourstory: ('src/pages/ourstory.html'),
+        login: ('src/pages/login.html'),
+        register: ('src/pages/register.html'),
+        admin: ('src/pages/admin.html'),
       },
     },
   },
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:3000",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
 });
